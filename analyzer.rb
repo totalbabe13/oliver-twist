cb87e142a56a6632483339d8d61ed1688e6ce6e7
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-18 18:53:19 -0500

    word count added

--- analyzer.rb
+++ analyzer.rb
@@ -5,3 +5,7 @@ text = lines.join
 puts "#{line_count} lines"
 total_characters = text.length
 puts "#{total_characters} characters"
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} characters excluding spaces"
+word_count = text.split.length
+puts "#{word_count} words"
